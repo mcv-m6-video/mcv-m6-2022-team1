f97dc08091d8e076afcaf752652da2decccd4790
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-03-22 12:00:45 +0100

    t2 task

--- w2/background_estimation.py
+++ w2/background_estimation.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2
 import numpy as np
 from typing import List, Tuple, Dict
@@ -7,7 +9,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from data import FrameLoader
-
 
 Bbox = Tuple[int, int, int, int]
 
@@ -52,6 +53,81 @@ class StillBackgroundEstimatorGrayscale:
 
     def set_tol(self, tol: float) -> None:
         self.tol = tol
+
+    def load_estimator(self, in_path: Path) -> None:
+        npz_arr = np.load(str(in_path))
+        self.mean = npz_arr["mean"]
+        self.variance = npz_arr["variance"]
+
+    def viz_estimator(self):
+        ax = plt.subplot()
+        plt.title("Background Estimator Standard Deviation")
+        im = ax.imshow(np.sqrt(self.variance), cmap="hot", interpolation="nearest")
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+
+        plt.colorbar(im, cax=cax)
+        plt.figure(dpi=150)
+
+        plt.show()
+        plt.close()
+
+
+class AdaptativeEstimatorGrayscale:
+    def __init__(self, loader: FrameLoader, tol: float = 2.5, p: float = 1.2) -> None:
+        self.loader = loader
+        self.tol = tol
+        self.imsize = self.loader.probe()
+
+        self.mean = np.zeros(self.imsize[:2])
+        self.variance = np.zeros(self.imsize[:2])
+        self.p = p
+
+    def __str__(self):
+        return f"Still Background estimator with {len(self.loader)} images"
+
+    def fit(self) -> None:
+        all_img = np.empty(
+            (self.imsize[0] * self.imsize[1], len(self.loader)),
+            dtype=float
+        )
+        for ii, img in tqdm(enumerate(self.loader), desc="Fit progress"):
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img = img.astype(float).flatten()
+            all_img[:, ii] = img
+
+        self.mean = all_img.mean(axis=-1).reshape((self.imsize[0], self.imsize[1]))
+        self.variance = all_img.var(axis=-1).reshape((self.imsize[0], self.imsize[1]))
+
+    def predict(self, img: np.ndarray):
+        img = img.astype(float)
+        mask = np.abs(img - self.mean) > (self.tol * (np.sqrt(self.variance) + 2))
+
+        print(f"mask before {mask}")
+
+        # FIXME: maybe not the fastest way
+        # calculate new mean and variance for all pixels
+        new_mean = self.p * img + (1 - self.p) * self.mean
+        new_variance = math.sqrt(self.p * (img - self.mean) ** 2 + (1 - self.p) * self.variance ** 2)
+
+        # replace only if it is background
+        self.mean = np.where(mask < 255, new_mean, self.mean)
+        self.variance = np.where(mask < 255, new_variance, self.variance)
+
+        return mask
+
+    def save_estimator(self, out_path: Path) -> None:
+        np.savez(
+            str(out_path),
+            mean=self.mean,
+            variance=self.variance
+        )
+
+    def set_tol(self, tol: float) -> None:
+        self.tol = tol
+
+    def set_p(self, p: float) -> None:
+        self.p = p
 
     def load_estimator(self, in_path: Path) -> None:
         npz_arr = np.load(str(in_path))
---
+++ w2/t2.py
@@ -0,0 +1,88 @@
+# ffmpeg -i ../vdo.avi %05d.jpg
+
+import cv2
+import json
+
+from tqdm.auto import tqdm
+from data import FrameLoader
+from pathlib import Path
+from background_estimation import StillBackgroundEstimatorGrayscale, AdaptativeEstimatorGrayscale, cleanup_mask, get_bboxes
+
+from viz import show_image, draw_bboxes
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+frame_path = Path(
+    "/home/cisu/PycharmProjects/mcv-m6-2022-team1/AICity_data/train/S03/c010/vdo_frames"
+)
+estimator_path = Path(
+    "/home/cisu/PycharmProjects/mcv-m6-2022-team1/AICity_data/train/S03/c010/estimators"
+)
+out_path = Path(
+    "/home/cisu/PycharmProjects/mcv-m6-2022-team1/AICity_data/train/"
+    "S03/c010/w2predictions"
+)
+gt_path = Path(
+    "/home/cisu/PycharmProjects/mcv-m6-2022-team1/AICity_data/train/"
+    "S03/c010/gt_coco"
+)
+
+train_loader = FrameLoader(frame_path, .25, "lower")
+test_loader = FrameLoader(frame_path, .75, "upper")
+
+estimator = AdaptativeEstimatorGrayscale(train_loader)
+
+estimator.fit()
+# For the sake of speed (and since we decided it was a good idea to fit the
+# entire training sequence on memory to generate statistics), we have enabled
+# a way to reload the estimated backgrounds.
+estimator.save_estimator(estimator_path / "estimator_adaptative.npz")
+estimator.load_estimator(estimator_path / "estimator_adaptative.npz")
+
+# interestingly enough, areas with high color happen to have higher std. This is
+# probably as a result of images not being 0-centered.
+estimator.viz_estimator()
+
+print("Testing...")
+
+# TODO grid search??
+tol_values = [0.5 * x for x in range(1, 11)]
+p_values = [0.5 * x for x in range(1, 11)]
+prediction = [[] for _ in range(len(tol_values))]
+
+for ii, (img_id, img) in tqdm(enumerate(test_loader), desc="Testing progress..."):
+    for jj, (tol, p) in enumerate(zip(tol_values, p_values)):
+        estimator.set_tol(tol)
+        estimator.set_p(p)
+        mask = estimator.predict(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY))
+
+        # A median filter + closing of size n works well enough. We need a heuristic
+        # to join close connected regions if they are significantly small. Our
+        # solution atm is to just purge any small bboxes.
+        mask = cleanup_mask(mask, 11)
+        bboxes = get_bboxes(mask, 50)
+
+        prediction[jj] += [{
+            "image_id": img_id,
+            "category_id": 1,
+            "bbox": list(x),
+            "score": 1.0
+        } for x in bboxes]
+
+    draw_bboxes(img, bboxes)
+    show_image(mask)
+    show_image(img)
+
+coco = COCO(str(gt_path / "gt_moving_onelabel.json"))
+
+for jj, tol in enumerate(tol_values):
+    with open(out_path / f"prediction_{jj}_adaptative.json", 'w') as f_pred:
+        json.dump(prediction[jj], f_pred)
+
+for jj, tol in enumerate(tol_values):
+    cocodt = coco.loadRes(prediction[jj])
+    cocoeval = COCOeval(coco, cocodt, 'bbox')
+
+    cocoeval.evaluate()
+    cocoeval.accumulate()
+    cocoeval.summarize()
