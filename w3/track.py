3eec819f610a3e86a010d2d2601133aae3346aa1
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-03-29 11:13:19 +0200

    overlap

--- w3/track.py
+++ w3/track.py
@@ -1,8 +1,11 @@
 class Track(object):
     def __init__(self, id: int, detections: int):
         self.id = id  # track id
-        self.detections = detections  # stores num of detections
         self.bbox = list()  # list of bbox associated to an object
+        self.frame_id_appearence = list()
 
     def append_bbox(self, new_bbox):
-        self.append(new_bbox)
+        self.bbox.append(new_bbox)
+
+    def append_frame_id_appearence(self, frame_id_):
+        self.frame_id_appearence.append(frame_id_)
