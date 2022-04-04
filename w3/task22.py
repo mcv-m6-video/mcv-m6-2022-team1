1e6a8e2952433c381efd8376871094ac305b81b0
ptorras <pautorras1@hotmail.com>
2022-04-04 23:28:12 +0200

    prepare w4 stuff and edits on w3

--- w3/task22.py
+++ w3/task22.py
@@ -1,19 +1,33 @@
+import csv
+
 from os import makedirs
 from pathlib import Path
  
-from track import read_detections, track_max_overlap, visualize_overlap, eval_file, track_KF, visualize_KF
+from track import read_detections, visualize_overlap, eval_file, track_KF, visualize_KF
 from data import FrameLoader
 import numpy as np
 import pandas as pd
 
-who = 'dani'
+who = 'pau'
 
 if who == 'dani':
-    detections_path = "E:\Master\M6 - Video analysis\Project/data/coco_instances_results.json"
-    frame_path = Path("E:\Master\M6 - Video analysis\Project/AICity_data/train/S03/c010/vdo_frames"    )
-    out_data = Path("E:\Master\M6 - Video analysis\Project/data")
+    detections_path = r"E:\Master\M6 - Video analysis\Project/data/" \
+                      r"coco_instances_results.json"
+    frame_path = Path(r"E:\Master\M6 - Video analysis\Project/"
+                      r"AICity_data/train/S03/c010/vdo_frames")
+    out_data = Path(r"E:\Master\M6 - Video analysis\Project/data")
     
     makedirs(out_data, exist_ok=True)
+
+elif who == 'pau':
+    detections_path = "/home/pau/Documents/master/M6/project/repo/w3/results/" \
+                      "train_holdout_aug/faster_fpn/coco_instances_results.json"
+    frame_path = Path("/home/pau/Documents/master/M6/project/data/AICity_data/"
+                      "AICity_data/train/S03/c010/vdo_frames")
+    out_data = Path("./results/watever")
+
+    makedirs(out_data, exist_ok=True)
+
 else:
     detections_path = "/home/cisu/PycharmProjects/mcv-m6-2022-team1/w3/data/coco_instances_results.json"
     frame_path = Path(
@@ -31,18 +45,12 @@ tracking_vel = track_KF(data, 536, 2141,  model_type = 0)
 # labels = ['image_id','category_id','id','left','top','width','height','score']
 labels = ['image_id','id','left','top','width','height','score','x','y','z']
 tracking_vel_pd = pd.DataFrame(tracking_vel,columns=labels)
-# tracking_acc_pd = pd.DataFrame(tracking_acc,columns=labels)
 
 print(len(tracking_vel))
 np.savez(out_data / "tracking_listKF.npz", tracking_vel)
-# tracking = np.load(out_data / "tracking_listKF.npz")
-# eval_file(track_list=tracking_vel, init_frame_id=536, last_frame_id=2141, csv_file=out_data / "KF_vel.cvs")
-# eval_file(track_list=tracking_acc_pd, init_frame_id=536, last_frame_id=2141, csv_file=out_data / "KF_acc.cvs")
 
-loader = FrameLoader(frame_path, .25, "upper")
-visualize_KF(tracking_vel, loader)
-
-import csv
+# loader = FrameLoader(frame_path, .25, "upper")
+# visualize_KF(tracking_vel, loader)
 
 # open the file in the write mode
 f = open(out_data / "tracking_KF.csv", 'w')
@@ -51,9 +59,8 @@ f = open(out_data / "tracking_KF.csv", 'w')
 writer = csv.writer(f)
 
 # write a row to the csv file
-writer.writerow(tracking_vel)
+for row in tracking_vel:
+    writer.writerow(row)
 
 # close the file
 f.close()
-
-# runfile('E:/Master/M6 - Video analysis/Project/mcv-m6-2022-team1/w3/eval_track.py',args =' "E:\Master\M6 - Video analysis\Project/data/annotations.xml" "E:\Master\M6 - Video analysis\Project/data/tracking_KF.csv" 536 2140')
---
+++ w4/track.py
@@ -0,0 +1,349 @@
+import json
+import csv
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import cv2
+from SORT import Sort
+from utils import iou, select_bboxes
+# from SORT_models import Sort
+
+
+class Track:
+    def __init__(self, track_id: int, first_bbox: np.ndarray, start_frame: int):
+        """
+        Initialise a track object. Bboxes must be in the XYXY format.
+
+        Parameters
+        ----------
+        track_id: int
+            Unique track identifier. Uniqueness is not enforced by the class
+            itself, but a wrapper object (handle with care!).
+        first_bbox: np.ndarray
+            First box to consider for the track (in XYXY format).
+        """
+        self.track_id = track_id
+        self.start_frame = start_frame
+        self.bboxes = [first_bbox]
+        self.centers = [self._get_center(first_bbox)]
+
+    def __str__(self):
+        return f"Track #{self.track_id} with {len(self.bboxes)} boxes\n"
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def _get_center(bbox):
+        return bbox.reshape((2, 2)).mean(0)
+
+    def append_bbox(self, new_bbox: np.ndarray):
+        self.bboxes.append(new_bbox)
+        self.centers.append(self._get_center(new_bbox))
+
+    def get_last_bbox(self):
+        return self.bboxes[-1]
+
+    def get_last_center(self):
+        return self.centers[-1]
+
+    def get_id(self):
+        return self.track_id
+
+    def cvt_mots(self):
+        return [[   # FIXME: Confidences should be added sooner or later
+            self.start_frame + ii, self.track_id, x[0], x[1], x[2] - x[0],
+            x[3] - x[1], 1, -1, -1, -1] for ii, x in enumerate(self.bboxes)
+        ]
+
+
+class MaxOverlapTracker:
+    def __init__(
+            self,
+            start_frame: int,
+            end_frame: int,
+            thresh: float = 0.5
+    ) -> None:
+        """
+        Given a detection input file in COCO format, this class performs
+        maximum overlap tracking of the detected objects.
+
+        Parameters
+        ----------
+        start_frame: int
+            First frame to consider
+        end_frame: int
+            Final frame to consider
+        thresh: float
+            IoU acceptance threshold
+        """
+        self.alive_tracks = []
+        self.dead_tracks = []
+
+        self.start_frame = start_frame
+        self.end_frame = end_frame
+
+        self.current_track = 0
+        self.thresh = thresh
+
+    def _numpy_alive_boxes(self) -> np.ndarray:
+        """
+        Get a Numpy representation of the set of boxes in tracks that are still
+        alive.
+
+        Returns
+        -------
+        np.ndarray
+            Set of "alive" boxes in XYXY format.
+
+        """
+        alive = [x.get_last_bbox() for x in self.alive_tracks]
+        alive = np.asarray(alive)
+
+        return alive
+
+    def _add_box_to_track(
+            self,
+            track_index: int,
+            new_bbox: np.ndarray
+    ) -> None:
+        """
+        Adds a new bounding box to an existing track.
+
+        Parameters
+        ----------
+        track_index: int
+            Index of the tracking object to append the box to.
+        new_bbox: np.ndarray
+            Bounding box to be appended in XYXY format.
+        """
+        self.alive_tracks[track_index].append_bbox(new_bbox)
+
+    def _kill_tracks(self, track_indices: list):
+        for ii in track_indices:
+            self.dead_tracks.append(self.alive_tracks[ii])
+        self.alive_tracks = [x for ii, x in enumerate(self.alive_tracks)
+                             if ii not in track_indices]
+
+    def _add_tracks(self, track_list: list):
+        for ii in track_list:
+            self.alive_tracks.append(ii)
+
+    def _merge_tracks(self) -> list:
+        return self.alive_tracks + self.dead_tracks
+
+    def track_objects(self, detections: list):
+        mots_style = coco2motsXYXY(detections)
+        first = np.asarray(
+            mots_style[mots_style["frame"] == self.start_frame]
+            [["left", "top", "right", "bot"]]
+        )
+
+        self.alive_tracks = [
+            Track(ii, bbox, self.start_frame) for ii, bbox in enumerate(first)
+        ]
+
+        self.current_track = len(self.alive_tracks)
+
+        for current_frame in range(self.start_frame + 1, self.end_frame + 1):
+            current_bboxes = np.asarray(
+                mots_style[mots_style["frame"] == current_frame]
+                [["left", "top", "right", "bot"]]
+            )
+
+            # TODO: NMS <here> could work
+
+            prev_bboxes = self._numpy_alive_boxes()
+            iou_boxes = iou(prev_bboxes, current_bboxes)
+            chosen_preds = select_bboxes(iou_boxes, self.thresh)
+
+            new_tracks = []
+
+            for pred, jj in enumerate(chosen_preds):
+                if jj >= 0:
+                    self._add_box_to_track(jj, current_bboxes[pred])
+                else:
+                    self.current_track += 1
+                    new_tracks.append(Track(
+                        self.current_track,
+                        current_bboxes[pred],
+                        current_frame
+                    ))
+
+            to_kill = np.where(
+                np.isin(np.arange(len(prev_bboxes)), chosen_preds, invert=True)
+            )[0].tolist()
+
+            self._kill_tracks(to_kill)
+            self._add_tracks(new_tracks)
+
+    def output_tracks(self, out_path: str):
+        all_tracks = self._merge_tracks()
+        all_mots = []
+        for track in all_tracks:
+            all_mots += track.cvt_mots()
+
+        pd.DataFrame(all_mots).to_csv(out_path, header=False, index=False)
+
+
+def read_detections(json_file: str) -> list:
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def coco2motsXYXY(
+        coco_detections: list,
+        conf_purge: float = None
+) -> pd.DataFrame:
+    rows = [
+        [x["image_id"], x["bbox"][0], x["bbox"][1], x["bbox"][0] + x["bbox"][2],
+         x["bbox"][1] + x["bbox"][3], x["score"]]
+        for x in coco_detections
+    ]
+    mots = pd.DataFrame(rows, columns=["frame", "left", "top", "right",
+                                       "bot", "confidence"])
+    mots = mots.sort_values(by="frame")
+    return mots
+
+
+def visualize_overlap(track_list, frame_loader, num_of_colors=200):
+    color_list = [tuple(np.random.choice(range(256), size=3)) for ic in range(num_of_colors)]
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video = cv2.VideoWriter("overlap.mp4", fourcc, 10, (1920, 1080))
+
+    trail_points = list()
+    trail_counter = 0
+    for img_frame_id, img in tqdm(frame_loader):
+        if img_frame_id == 900:
+            print("STOP")
+            break
+        img = np.array(img)
+        bboxes_to_draw = list()
+
+        for track in track_list:
+            # if track has stored the frame id, extract bbox and append to draw itvideo.write(img)
+            try:
+                index = track.frame_id_appearence.index(img_frame_id)
+                associated_id = track.id
+                bboxes_to_draw.append((track.bbox[index], associated_id))
+                trail_points.append((track.bbox[index], associated_id))
+            except ValueError:
+                pass
+
+        for (x, y, w, h), id_track in bboxes_to_draw:
+            cv2.circle(img, (int(x + (w / 2)), int(y + (h / 2))), 5, (255, 0, 0), -1)
+            cv2.putText(img, f"id: {id_track}", (int(x + (w / 2)), int(y + (h / 2))), cv2.FONT_HERSHEY_TRIPLEX, 1,
+                        (int(color_list[id_track % num_of_colors][0]), int(color_list[id_track % num_of_colors][1]),
+                         int(color_list[id_track % num_of_colors][2])), 2, cv2.LINE_AA)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        video.write(img)
+
+        # for (x, y, w, h), id_track in trail_points:
+        #     cv2.circle(img, (int(x + (w / 2)), int(y + (h / 2))), 5, (255, 0, 0), -1)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # video.write(img)
+        #
+        # video.write(img)
+        # if trail_counter%50 == 0:
+        #     trail_counter = 0
+        #     trail_points.clear()
+        # trail_counter += 1
+
+    cv2.destroyAllWindows()
+    video.release()
+
+
+def visualize_KF(track_list, frame_loader, num_of_colors=200):
+    color_list = [tuple(np.random.choice(range(256), size=3)) for ic in range(num_of_colors)]
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video = cv2.VideoWriter("overlap.mp4", fourcc, 10, (1920, 1080))
+
+    trail_points = list()
+    trail_counter = 0
+    for img_frame_id, img in tqdm(frame_loader):
+        if img_frame_id == 900:
+            print("STOP")
+            break
+        img = np.array(img)
+        bboxes_to_draw = list()
+
+        for track in track_list:
+            # if track has stored the frame id, extract bbox and append to draw itvideo.write(img)
+            try:
+                index = track[0]
+                associated_id = track[1]
+                bboxes_to_draw.append((track[2:6], associated_id))
+                trail_points.append((track[2:6], associated_id))
+            except ValueError:
+                pass
+
+        for (x, y, w, h), id_track in bboxes_to_draw:
+            cv2.circle(img, (int(x + (w / 2)), int(y + (h / 2))), 5, (255, 0, 0), -1)
+            cv2.putText(img, f"id: {id_track}", (int(x + (w / 2)), int(y + (h / 2))), cv2.FONT_HERSHEY_TRIPLEX, 1,
+                        (int(color_list[id_track % num_of_colors][0]), int(color_list[id_track % num_of_colors][1]),
+                         int(color_list[id_track % num_of_colors][2])), 2, cv2.LINE_AA)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        video.write(img)
+
+        # for (x, y, w, h), id_track in trail_points:
+        #     cv2.circle(img, (int(x + (w / 2)), int(y + (h / 2))), 5, (255, 0, 0), -1)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # video.write(img)
+        #
+        # video.write(img)
+        # if trail_counter%50 == 0:
+        #     trail_counter = 0
+        #     trail_points.clear()
+        # trail_counter += 1
+
+    cv2.destroyAllWindows()
+    video.release()
+
+
+def eval_file(track_list, init_frame_id, last_frame_id, csv_file):
+    for frame_id in tqdm(range(init_frame_id, last_frame_id + 1)):
+        for track in track_list:
+            try:
+                index = track.frame_id_appearence.index(frame_id)
+                id_csv = track.id
+                bbox = track.bbox[index]
+                csv_row = [str(frame_id), str(id_csv), str(bbox[0]), str(bbox[1]), str(bbox[2]), str(bbox[3]), "-1",
+                           "-1", "-1", "-1"]
+                with open(csv_file, 'a') as fd:
+                    writer = csv.writer(fd)
+                    writer.writerow(csv_row)
+            except ValueError:
+                pass
+
+
+def track_KF(data, init_frame_id, last_frame_id, IoU_threshold=0.2, score_threshold =0.9, model_type = 0):
+    # Assumes first frame as initialization
+
+    bb_id_updated = []
+
+    # tracker = Sort(model_type = model_type)
+    tracker = Sort()
+    
+    for frame_id in tqdm(range(init_frame_id, last_frame_id + 1)):
+
+        frame_detections = [x for x in data if x["image_id"] == frame_id]
+        frame_detections = [x for x in frame_detections if x["score"] > score_threshold]
+
+        a = np.array([x['bbox'] for x in frame_detections])
+        b = np.array([x['score'] for x in frame_detections])
+        b = b.reshape((b.shape[0], 1))
+        dets = np.append(a, b, axis=1)
+        dets[:, 2:4] += dets[:, 0:2]
+        
+        trackers = tracker.update(dets)
+        
+        for bb_dets, bb_update in zip(frame_detections, trackers):
+            bb_id_updated.append([
+                bb_dets['image_id'], int(bb_update[4])-1,
+                bb_update[0], bb_update[1], bb_update[2]-bb_update[0],
+                bb_update[3]-bb_update[1], -1, -1, -1, -1
+            ])
+        
+
+    return bb_id_updated
