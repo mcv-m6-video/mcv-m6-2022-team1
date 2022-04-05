47029a3bfa8eecdcfef41ed3b41db9cd11df2504
ptorras <pautorras1@hotmail.com>
2022-04-05 12:06:53 +0200

    bash scripts

--- w4/bash/run_train.sh
+++ w4/bash/run_train.sh
@@ -1,18 +1,91 @@
 dataset_path=/home/pau/Documents/datasets/aicity/train/
-output_path=/home/pau/Documents/master/M6/project/repo/w4/results/train_models
+output_path=/home/pau/Documents/master/M6/project/repo/w4/results/train_models_testS03
+train="S01 S04"
+test="S03"
 
 python3 ../train_archs.py $dataset_path \
                           $output_path/faster_fpn \
-                          COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml
+                          COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
 
 python3 ../train_archs.py $dataset_path \
                           $output_path/faster_dc5 \
-                          COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml
+                          COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
 
 python3 ../train_archs.py $dataset_path \
                           $output_path/faster_c4 \
-                          COCO-Detection/faster_rcnn_R_50_C4_3x.yaml
+                          COCO-Detection/faster_rcnn_R_50_C4_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
 
 python3 ../train_archs.py $dataset_path \
                           $output_path/retina_fpn \
-                          COCO-Detection/retinanet_R_50_FPN_1x.yaml
+                          COCO-Detection/retinanet_R_50_FPN_1x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+
+dataset_path=/home/pau/Documents/datasets/aicity/train/
+output_path=/home/pau/Documents/master/M6/project/repo/w4/results/train_models_testS01
+train="S03 S04"
+test="S01"
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/faster_fpn \
+                          COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/faster_dc5 \
+                          COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/faster_c4 \
+                          COCO-Detection/faster_rcnn_R_50_C4_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/retina_fpn \
+                          COCO-Detection/retinanet_R_50_FPN_1x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+dataset_path=/home/pau/Documents/datasets/aicity/train/
+output_path=/home/pau/Documents/master/M6/project/repo/w4/results/train_models_testS04
+train="S01 S03"
+test="S04"
+
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/faster_fpn \
+                          COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/faster_dc5 \
+                          COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/faster_c4 \
+                          COCO-Detection/faster_rcnn_R_50_C4_3x.yaml \
+                          --train_on $train \
+                          --test_on $test
+
+
+python3 ../train_archs.py $dataset_path \
+                          $output_path/retina_fpn \
+                          COCO-Detection/retinanet_R_50_FPN_1x.yaml \
+                          --train_on $train \
+                          --test_on $test
