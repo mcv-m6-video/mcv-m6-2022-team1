c6547a996b7d19c92755ae3e00577e5770a4ddc7
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-04-24 12:02:08 +0200

    renamed

---
+++ w5/build_data_utils.py
@@ -0,0 +1,52 @@
+import csv
+import json
+import os.path
+
+import cv2
+import pandas as pd
+
+
+def detections_txt2Json(tracks_txt_file, output_file_json):
+    json_list = []
+    with open(tracks_txt_file) as csvFile:
+        data = {}
+        csvReader = csv.reader(csvFile)
+        for rows in csvReader:
+            data["image_id"] = int(rows[0])
+            data["bbox"] = [float(rows[2]), float(rows[3]), float(rows[4]), float(rows[5])]
+            data["category_id"] = 1
+            data["score"] = float(rows[6])
+            json_list.append(data)
+
+    with open(output_file_json, 'w') as fout:
+        json.dump(json_list, fout)
+
+
+def create_data_metric_learning(tracks_csv, frame_path, out_path):
+    # read track csv
+    df = pd.read_csv(tracks_csv, sep=",", header=None,
+                     names=["frame", "ID", "left", "top", "width", "height", "confidence", "null1", "null2", "null3"])
+
+    num_frames = df["frame"].max()
+
+    # go frame by frame
+    for i in range(1, num_frames + 1):
+        detects_for_frame = df.loc[df['frame'] == i]
+
+        img = cv2.imread(os.path.join(frame_path, "" + str(i) + ".jpg"))
+
+        count = 0
+
+        for indx in detects_for_frame.index:
+            (x, y, w, h) = (
+                detects_for_frame['left'][indx], detects_for_frame['top'][indx], detects_for_frame['width'][indx],
+                detects_for_frame['height'][indx])
+
+            detection_ID = detects_for_frame["ID"][indx]
+
+            store_path = os.path.join(out_path, detection_ID)
+
+            os.makedirs(store_path, exist_ok=True)  # every id in a different folder
+
+            cropped_image = img[y:y + h, x:x + w]
+            cv2.imwrite(os.path.join(store_path, str(count) + ".jpg"), cropped_image)
